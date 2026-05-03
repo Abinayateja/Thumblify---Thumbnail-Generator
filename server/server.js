2295e3132fb832f51784c28eded54bef2d5bc6a1
Abinayateja <gaddamabinayateja@gmail.com>
2026-05-03 12:05:14 +0530

    Fix Render port binding

--- server/server.js
+++ server/server.js
@@ -15,12 +15,21 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
-    credentials: true
-  })
-);
+app.use(cors({
+  origin: function (origin, callback) {
+    const allowedOrigins = [
+      "https://thumblifyabi.netlify.app"
+    ];
+
+    // allow requests with no origin (like Postman)
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
